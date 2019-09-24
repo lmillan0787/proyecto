aeaf42768a76951059e9c19df36c5b7d192892c1
lmillan0787 <lmillan0787@gmail.com>
2019-09-23 21:44:22 -0400

    23092

--- models/vistasModelo.php
+++ models/vistasModelo.php
@@ -2,11 +2,11 @@
 
     class vistasModelo{
         protected function obtener_vistas_modelo($vistas){
-<<<<<<< HEAD
+
             $listaBlanca = ["home","eventos","editarEvento","registrarEvento","regPueblo","deportistas","delegados","medicos","invitados","pertec","reg_usr","principal","personas","registrarPersona","editarPersona","usuarios","regDeportista","vistaPrueba","regDelegado","regMedico","regInvitado","regInvitadoE","participacion","regParticipacion","tecnicos","registrarTecnico"];
-=======
-            $listaBlanca = ["home","eventos","editarEvento","registrarEvento","regPueblo","deportistas","delegados","medicos","invitados","pertec","reg_usr","principal","personas","registrarPersona","editarPersona","usuarios","regDeportista","vistaPrueba","regDelegado","regMedico","regInvitado","regInvitadoE","participacion","regParticipacion"];
->>>>>>> bccd9cdb90572ceba500c982ea01407e78905c1a
+
+        
+
             if(in_array($vistas, $listaBlanca)){
                 if(is_file("./views/contenidos/".$vistas.".php")){
                     $contenido="./views/contenidos/".$vistas.".php";
